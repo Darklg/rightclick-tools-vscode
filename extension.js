5033c41434b7505e3aab49d1b660c409a02fc2a1
Kevin <kevin@rocher-dev.com>
2026-04-14 16:01:38 +0200

    v 0.0.1

---
+++ extension.js
@@ -0,0 +1,95 @@
+const vscode = require('vscode');
+const path = require('path');
+const {
+    exec
+} = require('child_process');
+
+function getUri(uri) {
+    if (uri && uri.fsPath) {
+        return uri;
+    }
+
+    const editor = vscode.window.activeTextEditor;
+    if (editor) {
+        return editor.document.uri;
+    }
+
+    throw new Error('No file context available');
+}
+
+/**
+ * Get directory path from URI
+ * @param {vscode.Uri} uri
+ * @returns {string}
+ */
+function getDirectory(uri) {
+    if (!uri || !uri.fsPath) {
+        throw new Error('Invalid URI');
+    }
+
+    const fsPath = uri.fsPath;
+    return path.extname(fsPath) ? path.dirname(fsPath) : fsPath;
+}
+
+/**
+ * Open folder in Finder (macOS)
+ * @param {string} dir
+ */
+function openInFinder(dir) {
+    exec(`open "${dir}"`, (err) => {
+        if (err) {
+            vscode.window.showErrorMessage(`Failed to open Finder: ${err.message}`);
+        }
+    });
+}
+
+/**
+ * Open folder in VSCode terminal
+ * @param {string} dir
+ */
+function openInTerminal(dir) {
+    const terminal = vscode.window.createTerminal({
+        name: 'Folder Terminal',
+        cwd: dir
+    });
+
+    terminal.show();
+}
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+    const openFinderCmd = vscode.commands.registerCommand(
+        'extension.openInFinder',
+        (uri) => {
+            try {
+                const dir = getDirectory(getUri(uri));
+                openInFinder(dir);
+            } catch (e) {
+                vscode.window.showErrorMessage(e.message);
+            }
+        }
+    );
+
+    const openTerminalCmd = vscode.commands.registerCommand(
+        'extension.openInTerminal',
+        (uri) => {
+            try {
+                const dir = getDirectory(getUri(uri));
+                openInTerminal(dir);
+            } catch (e) {
+                vscode.window.showErrorMessage(e.message);
+            }
+        }
+    );
+
+    context.subscriptions.push(openFinderCmd, openTerminalCmd);
+}
+
+function deactivate() {}
+
+module.exports = {
+    activate,
+    deactivate
+};
